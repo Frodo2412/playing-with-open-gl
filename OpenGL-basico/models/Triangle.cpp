8d02fe158604e4dd1705d8667ae2397e61cc53f3
Bruno <brunolemus553@gmail.com>
2024-03-29 19:52:31 -0300

    feat: `triangle` definition and impl

---
+++ OpenGL-basico/models/Triangle.cpp
@@ -0,0 +1,31 @@
+﻿#include "Triangle.h"
+
+vector triangle::get_a() const
+{
+    return a_;
+}
+
+vector triangle::get_b() const
+{
+    return b_;
+}
+
+vector triangle::get_c() const
+{
+    return c_;
+}
+
+void triangle::set_a(const vector& new_a)
+{
+    a_ = new_a;
+}
+
+void triangle::set_b(const vector& new_b)
+{
+    b_ = new_b;
+}
+
+void triangle::set_c(const vector& new_c)
+{
+    c_ = new_c;
+}
---
+++ OpenGL-basico/models/Triangle.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "vector.h"
+
+class triangle
+{
+    vector a_, b_, c_;
+
+public:
+    // Constructor
+    explicit triangle(const vector& a = vector(0, 0, 0), const vector& b = vector(0, 0, 0),
+                      const vector& c = vector(0, 0, 0)) : a_(a), b_(b), c_(c)
+    {
+    }
+
+    // Getter methods for the vertexes
+    vector get_a() const;
+    vector get_b() const;
+    vector get_c() const;
+
+    // Setter methods for the coordinates
+    void set_a(const vector& new_a);
+    void set_b(const vector& new_b);
+    void set_c(const vector& new_c);
+};
