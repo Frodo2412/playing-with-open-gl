d13a2f173572975ef36c48c521f09c00a2c33d83
sofiu <156955245+sofiieww@users.noreply.github.com>
2024-04-15 18:53:50 -0300

    Pausa (#20)
    * pausa
    
    * pausa 2
    
    * Update main.cpp
    
    Co-authored-by: Bruno Lemus <78759358+Frodo2412@users.noreply.github.com>
    
    * Update main.cpp
    
    Co-authored-by: Bruno Lemus <78759358+Frodo2412@users.noreply.github.com>
    
    * pause3
    
    ---------
    
    Co-authored-by: Bruno Lemus <78759358+Frodo2412@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 #include "OpenGL-basico/geometry//vector.h"
 #include "OpenGL-basico/geometry/grid.h"
+#include "OpenGL-basico/geometry/square.h"
 #include "OpenGL-basico/textures/texture.h"
 #include "OpenGL-basico/textures/texture_loader.h"
 #include "OpenGL-basico/utils/camera.h"
@@ -27,7 +28,7 @@ int main(int argc, char* argv[])
     SDL_Window* win = SDL_CreateWindow("ICG-UdelaR",
                                        SDL_WINDOWPOS_CENTERED,
                                        SDL_WINDOWPOS_CENTERED,
-                                       1920, 1080, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+                                       640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     SDL_GLContext context = SDL_GL_CreateContext(win);
 
     glMatrixMode(GL_PROJECTION);
@@ -40,7 +41,6 @@ int main(int argc, char* argv[])
     glMatrixMode(GL_MODELVIEW);
 
     bool fin = false;
-
     clock::init();
     SDL_Event event;
 
@@ -54,6 +54,11 @@ int main(int argc, char* argv[])
     const auto bricks_texture = texture_loader::load_texture("../assets/bricks_1.jpg");
     const auto some_block = cube(1, vector(0, 0, 0));
 
+    
+    //const auto pause_texture = texture_loader::load_texture("../assets/pausa.png");
+    //const auto pantallaPausa = square(vector(-320,-240,1), vector(320,-240,1), vector(320,240,1), vector(-320,240,1));
+    
+    
     do
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -70,24 +75,30 @@ int main(int argc, char* argv[])
 
         renderer::draw(floor, grass_texture);
         renderer::draw(some_block, bricks_texture);
+        
+        //renderer::draw(pantallaPausa, pause_texture);
 
+        float elapsed_time = static_cast<float>(clock::get_ticks());
+        
         //MANEJO DE EVENTOS
-        while (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event) )
         {
             switch (event.type)
             {
             case SDL_MOUSEWHEEL:
-                if (event.wheel.y > 0) camera.zoom_in(0.1f);
-                else camera.zoom_out(0.1f);
+                    if (event.wheel.y > 0)
+                        camera.zoom_in(0.1f);
+                    else
+                        camera.zoom_out(0.1f);
                 break;
             case SDL_MOUSEMOTION:
                 {
-                    float x_offset = static_cast<float>(event.motion.xrel);
-                    float y_offset = -static_cast<float>(event.motion.yrel);
+                    float x_offset = static_cast<float>(event.motion.xrel) * elapsed_time;
+                    float y_offset = -static_cast<float>(event.motion.yrel) * elapsed_time;
                     std::cout << "Mouse movement: " << x_offset << ", " << y_offset << "\n";
                     camera.rotate(x_offset, y_offset);
-                    break;
                 }
+                break;
             case SDL_QUIT:
                 fin = true;
                 break;
@@ -95,30 +106,34 @@ int main(int argc, char* argv[])
                 switch (event.key.keysym.sym)
                 {
                 case SDLK_a:
-                    std::cout << "LEFT\n";
-                    displacement.set_x(0.1f);
+                        std::cout << "LEFT\n";
+                        displacement.set_x(0.1f * elapsed_time);
                     break;
                 case SDLK_d:
-                    std::cout << "RIGHT\n";
-                    displacement.set_x(-0.1f);
+                        std::cout << "RIGHT\n";
+                        displacement.set_x(-0.1f * elapsed_time);
                     break;
                 case SDLK_w:
-                    std::cout << "UP\n";
-                    displacement.set_z(0.1f);
+                        std::cout << "UP\n";
+                        displacement.set_z(0.1f * elapsed_time);
                     break;
                 case SDLK_s:
-                    std::cout << "DOWN\n";
-                    displacement.set_z(-0.1f);
+                        std::cout << "DOWN\n";
+                        displacement.set_z(-0.1f * elapsed_time);
                     break;
+                case SDLK_p:
+                        std::cout << "PAUSE\n";
+                        clock::toggle_pause();
+                        break;
                 default: break;
                 }
             case SDL_KEYUP:
                 switch (event.key.keysym.sym)
                 {
-                case SDLK_ESCAPE:
-                    fin = true;
+            case SDLK_q:
+                fin = true;
                     break;
-                default: break;
+            default: break;
                 }
             default: break;
             }
