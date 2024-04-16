da84500d88ba4fafa714f6dd2594273648255479
matiasencion <108829786+matiasencion@users.noreply.github.com>
2024-04-16 18:57:53 -0300

    Camrra arriba 2 (#21)
    * agregando modos de camara y funcion de set a camera
    
    * added a new camera because it causes problems with the first person
    
    * now BOMBERMAN move along with all the cameras
    
    * la posicion de bomberan esta bien, el problema es que el punto rojo no se actualiza en first person
    
    * ahora es una sola camara, tambien puse para que en primera persona se mueva la camara cuando mantenes el click de momento
    
    * ahora si esta todo dentro de la clase camera y anda todo, en primera persona le puse para que se mueva la cam con click,(es momentaneo para probar y eso) si no les gusta se los cambio
    
    * ahora si esta todo dentro de la clase camera y anda todo, en primera persona le puse para que se mueva la cam con click,(es momentaneo para probar y eso) si no les gusta se los cambioa
    
    * conflictos resueltos

--- OpenGL-basico/utils/camera.h
+++ OpenGL-basico/utils/camera.h
@@ -1,24 +1,54 @@
 #pragma once
 #include "../geometry/vector.h"
 
+enum CameraMode
+{
+    first,
+    original,
+    perspective
+};
+
 class camera
 {
     vector position_, direction_, up_;
+    CameraMode mode_;
+    bool move_camera_first_;
+    float perspective_zoom_;
 
 public:
     explicit camera(const vector& position, const vector& direction, const vector& up)
         : position_(position), direction_(direction), up_(up)
     {
+        mode_ = CameraMode::first;
+        move_camera_first_ = false;
+        perspective_zoom_ = 0;
     }
 
     explicit camera(const float x, const float y, const float z)
         : position_(vector(x, y, z)), direction_(vector(0, 0, 0)), up_(vector(0, 1, 0))
     {
+        mode_ = CameraMode::first;
+        move_camera_first_ = false;
+        perspective_zoom_ = 0;
     }
 
     vector get_position() const;
     vector get_direction() const;
     vector get_up() const;
+    CameraMode get_mode();
+    bool get_move_camera_first();
+    float get_perspective_zoom();
+
+    void set_position(vector pos);
+
+    void set_direction(vector dir);
+
+    void set_up(vector up);
+    
+    void set_mode(CameraMode mode);
+
+    void set_move_camera_first(bool move_camera_first);
+    void set_perspective_zoom(float perspective_zoom);
 
     void move(const vector& displacement);
 
@@ -26,7 +56,7 @@ public:
     void zoom_out(float amount);
     
     void rotate(float x_offset, float y_offset);
-
-    void refresh() const;
+    
+    void refresh(vector bomber_man);
     
 };
--- main.cpp
+++ main.cpp
@@ -44,9 +44,8 @@ int main(int argc, char* argv[])
     clock::init();
     SDL_Event event;
 
+    auto bomber_man = vector(0, 0, 5);
     auto camera = ::camera(0, 0, 5);
-
-    auto displacement = vector(0, 0, 0);
 
     const auto grass_texture = texture_loader::load_texture("../assets/grass_1.jpg");
     const auto floor = grid(10, 10, 1, vector(0, 1, 0));
@@ -54,30 +53,35 @@ int main(int argc, char* argv[])
     const auto bricks_texture = texture_loader::load_texture("../assets/bricks_1.jpg");
     const auto some_block = cube(1, vector(0, 0, 0));
 
+    /*SDL_SetWindowGrab(win, SDL_TRUE);
+    SDL_ShowCursor(SDL_TRUE);//SDL_FALSE PARA OCULTAR MOUSE*/
+
     
     //const auto pause_texture = texture_loader::load_texture("../assets/pausa.png");
     //const auto pantallaPausa = square(vector(-320,-240,1), vector(320,-240,1), vector(320,240,1), vector(-320,240,1));
-    
+   
     
     do
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
 
-        camera.move(displacement);
-        const auto camera_position = camera.get_position();
-        const auto camera_direction = camera.get_direction();
-        const auto camera_up = camera.get_up();
-        gluLookAt(camera_position.get_x(), camera_position.get_y(), camera_position.get_z(),
-                  camera_direction.get_x(), camera_direction.get_y(), camera_direction.get_z(),
-                  camera_up.get_x(), camera_up.get_y(), camera_up.get_z());
-        displacement.reset();
-
+        camera.refresh(bomber_man);
+        gluLookAt(camera.get_position().get_x(), camera.get_position().get_y(), camera.get_position().get_z(),
+              camera.get_direction().get_x(), camera.get_direction().get_y(), camera.get_direction().get_z(),
+              camera.get_up().get_x(), camera.get_up().get_y(), camera.get_up().get_z());
         renderer::draw(floor, grass_texture);
         renderer::draw(some_block, bricks_texture);
         
         //renderer::draw(pantallaPausa, pause_texture);
 
+        //BOMBERMAN MOMENTANEO
+        glPointSize(20);
+        glBegin(GL_POINTS);
+            glColor3f(1,0,0);
+            glVertex3f(bomber_man.get_x(), bomber_man.get_y(), bomber_man.get_z());
+        glEnd();
+      
         float elapsed_time = static_cast<float>(clock::get_ticks());
         
         //MANEJO DE EVENTOS
@@ -86,18 +90,29 @@ int main(int argc, char* argv[])
             switch (event.type)
             {
             case SDL_MOUSEWHEEL:
-                    if (event.wheel.y > 0)
-                        camera.zoom_in(0.1f);
-                    else
-                        camera.zoom_out(0.1f);
+                if (event.wheel.y > 0) camera.zoom_in(0.1f);
+                if ((event.wheel.y>0 && camera.get_perspective_zoom() < 2) || (event.wheel.y<0 && camera.get_perspective_zoom() > -5))
+                {
+                    camera.set_perspective_zoom(camera.get_perspective_zoom() + event.wheel.y * 0.20f);
+                }
+                else camera.zoom_out(0.1f);
                 break;
             case SDL_MOUSEMOTION:
+                if (event.button.button == SDL_BUTTON_LEFT)
                 {
-                    float x_offset = static_cast<float>(event.motion.xrel) * elapsed_time;
-                    float y_offset = -static_cast<float>(event.motion.yrel) * elapsed_time;
+                    float x_offset = static_cast<float>(event.motion.xrel);
+                    float y_offset = -static_cast<float>(event.motion.yrel);
                     std::cout << "Mouse movement: " << x_offset << ", " << y_offset << "\n";
                     camera.rotate(x_offset, y_offset);
+                    camera.set_move_camera_first(true);
+                } else
+                {
+                    camera.set_move_camera_first(false);
                 }
+                if (event.wheel.y > 0)
+                    camera.zoom_in(0.1f);
+                else
+                    camera.zoom_out(0.1f);
                 break;
             case SDL_QUIT:
                 fin = true;
@@ -106,21 +121,37 @@ int main(int argc, char* argv[])
                 switch (event.key.keysym.sym)
                 {
                 case SDLK_a:
-                        std::cout << "LEFT\n";
-                        displacement.set_x(0.1f * elapsed_time);
+                    std::cout << "LEFT\n";
+                    bomber_man.set_x(bomber_man.get_x() - 0.1f);
                     break;
                 case SDLK_d:
-                        std::cout << "RIGHT\n";
-                        displacement.set_x(-0.1f * elapsed_time);
+                    std::cout << "RIGHT\n";
+                    bomber_man.set_x(bomber_man.get_x() + 0.1f);
                     break;
                 case SDLK_w:
-                        std::cout << "UP\n";
-                        displacement.set_z(0.1f * elapsed_time);
+                    std::cout << "UP\n";
+                    bomber_man.set_z(bomber_man.get_z() - 0.1f);
                     break;
                 case SDLK_s:
-                        std::cout << "DOWN\n";
-                        displacement.set_z(-0.1f * elapsed_time);
+                    std::cout << "DOWN\n";
+                    bomber_man.set_z(bomber_man.get_z() + 0.1f);
                     break;
+                case SDLK_v:
+                    switch (camera.get_mode())
+                    {
+                        case CameraMode::first:
+                            std::cout << "CAMERA CHANGED TO ORIGINAL\n";
+                            camera.set_mode(CameraMode::original);
+                            break;
+                        case CameraMode::original:
+                            std::cout << "CAMERA CHANGED TO PERSPECTIVE\n";
+                            camera.set_mode(CameraMode::perspective);
+                            break;
+                        case CameraMode::perspective:
+                            std::cout << "CAMERA CHANGED TO FIRST PERSON\n";
+                            camera.set_mode(CameraMode::first);
+                            break;
+                    }
                 case SDLK_p:
                         std::cout << "PAUSE\n";
                         clock::toggle_pause();
