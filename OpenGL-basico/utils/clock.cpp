ebeaaa6e0d6e52cf2b1812aafae92b42f296513a
Matias Sencion <matiassencion@gmail.com>
2024-05-03 21:33:33 -0300

    variable que sobraba

--- OpenGL-basico/utils/clock.cpp
+++ OpenGL-basico/utils/clock.cpp
@@ -32,8 +32,7 @@ void clock::reset()
 
 Uint32 clock::get_total_time()
 {
-    const Uint32 current_time = instance_->played_time_;
-    return current_time;
+    return instance_->played_time_;
 }
 
 bool clock::get_is_paused() const
