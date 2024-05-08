0c9e836739f3efc74ede8e758d6723cc6a92651d
Matias Sencion <matiassencion@gmail.com>
2024-05-08 12:53:17 -0300

    clase particle y clase vector4 para el color

---
+++ OpenGL-basico/entities/particle.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "../geometry/vector3.h"
+#include "../geometry/vector4.h"
+
+
+class particle
+{
+private:
+    vector3 position_;
+    vector3 velocity_;
+    vector4 color_;
+    float life_time_;
+    
+public:
+    
+};
